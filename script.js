36387826f68ab54e317b25c7896da16bb1313eeb
MthoNtanzi <mthontanzi99@gmail.com>
2026-04-16 14:40:27 +0200

    -update: make navbar responsive -make about and index pages responsive

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.getElementById("donationForm")?.addEventListener("submit", (e) => {
     e.preventDefault();
     alert("Thank you for your donation!");
 });
+
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("navMenu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
