5026f130e33a146b66a3a6e9a82df1b22e941fb8
MthoNtanzi <mthontanzi99@gmail.com>
2026-04-21 21:14:40 +0200

    Make sponsor and donate page responsive

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ const menu = document.getElementById("navMenu");
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+// Modals
+
+// Open modal when card is clicked
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+        const modalId = card.dataset.modal;
+        document.getElementById(modalId).classList.add("show");
+    });
+});
+
+// Close modal (X button)
+document.querySelectorAll(".close-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.closest(".modal").classList.remove("show");
+    });
+});
+
+// Close when clicking outside
+document.querySelectorAll(".modal").forEach(modal => {
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.classList.remove("show");
+        }
+    });
+});
